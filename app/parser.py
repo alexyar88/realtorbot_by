6e95971cf868e2dda674a53956c1e8bce731adee
alyar88 <lsd00000>
2019-10-04 00:24:49 +0300

    initial commit

---
+++ app/parser.py
@@ -0,0 +1,96 @@
+import requests
+import time
+import csv
+import os
+from bs4 import BeautifulSoup
+from datetime import datetime
+from pathlib import Path
+
+from flask import Blueprint, render_template, request, jsonify
+
+
+csv_path = str(Path(__file__).parent.absolute()) + '/../data/'
+
+is_docker = os.environ.get('IS_DOCKER', False)
+if is_docker:
+    csv_path = '/mounted/data/'
+
+
+parser = Blueprint('parser', __name__)
+
+@parser.route('/parse-and-save-csv')
+def parse_and_save_csv():
+    time_string = datetime.now().strftime('%Y_%m_%d_%H_%M_%S')
+
+    columns = ['id', 'author_id', 'address', 'user_address', 'latitude', 'longitude', 'resale', 'floor', 'number_of_floors', 'number_of_rooms', 'area_total', 'area_living', 'area_kitchen', 'seller', 'created_at',
+               'last_time_up', 'url',
+               'photo', 'option1', 'option2', 'option3', 'option4', 'price_byn', 'price_usd']
+
+    with open(csv_path + 'flats_minsk_{}.csv'.format(time_string), 'w', newline='') as file:
+        writer = csv.writer(file, delimiter=';')
+        writer.writerow(columns)
+
+        for area in range(1, 1001):
+
+            page_num = 1
+            page_last = 10000
+
+            while True:
+                page_result_json = requests.get(get_page_minsk(area, area + 1, page_num)).json()
+                page_last = page_result_json['page']['last']
+
+                print('Page:', page_num, ', Area: ', area)
+                print(page_result_json['page'])
+
+                flats = page_result_json['apartments']
+                parse_flats(flats, writer)
+                page_num += 1
+
+                if page_num > page_last:
+                    break
+
+    print('Done!')
+
+
+def parse_flats(flats, writer):
+    time.sleep(0.06)
+    for flat in flats:
+        flat.setdefault('')
+        page = requests.get(flat.get('url'))
+        parser = BeautifulSoup(page.content, 'html.parser')
+        flat_options = parser.select('ul.apartment-options li.apartment-options__item')
+        options = [option.text for option in flat_options]
+        if len(options) < 4:
+            print('Skipped: ', flat.get('url'))
+            continue
+        flat_arr = [
+            flat.get('id'),
+            flat.get('author_id'),
+            flat.get('location', dict()).get('address', ''),
+            flat.get('location', dict()).get('user_address', ''),
+            flat.get('location', dict()).get('latitude', ''),
+            flat.get('location', dict()).get('longitude', ''),
+            flat.get('resale'),
+            flat.get('floor'),
+            flat.get('number_of_floors'),
+            flat.get('number_of_rooms'),
+            flat.get('area', dict()).get('total', ''),
+            flat.get('area', dict()).get('living', ''),
+            flat.get('area', dict()).get('kitchen', ''),
+            flat.get('seller', dict()).get('type', ''),
+            flat.get('created_at'),
+            flat.get('last_time_up'),
+            flat.get('url'),
+            flat.get('photo'),
+            options[0],
+            options[1],
+            options[2],
+            options[3],
+            flat.get('price', dict()).get('converted', dict()).get('BYN', dict()).get('amount', 0),
+            flat.get('price', dict()).get('converted', dict()).get('USD', dict()).get('amount', 0),
+        ]
+        writer.writerow(flat_arr)
+
+def get_page_minsk(area1, area2, n):
+    return "https://pk.api.onliner.by/search/apartments?area%5Bmin%5D={}&area%5Bmax%5D={}&bounds%5Blb%5D%5Blat%5D=53.820922446131&bounds%5Blb%5D%5Blong%5D=27.344970703125&bounds%5Brt%5D%5Blat%5D=53.97547425743&bounds%5Brt%5D%5Blong%5D=27.77961730957&v=0.12052430637493972&page={}".format(
+        area1, area2, n)
---
+++ app/train_model.py
@@ -0,0 +1,156 @@
+import requests
+import time
+import csv
+import os
+import joblib
+from bs4 import BeautifulSoup
+from datetime import datetime
+from pathlib import Path
+
+from custom_transformer import CustomTransformer
+from sklearn.ensemble import RandomForestRegressor, VotingRegressor
+from sklearn.preprocessing import OneHotEncoder, MinMaxScaler
+from sklearn.pipeline import Pipeline
+from sklearn.compose import make_column_transformer, ColumnTransformer
+from sklearn.base import BaseEstimator, TransformerMixin
+from lightgbm.sklearn import LGBMRegressor
+from xgboost.sklearn import XGBRegressor
+
+import numpy as np
+import pandas as pd
+
+
+from flask import Blueprint, render_template, request, jsonify
+
+
+csv_path = str(Path(__file__).parent.absolute()) + '/../data/'
+model_path = str(Path(__file__).parent.absolute()) + '/../ml_models/'
+
+is_docker = os.environ.get('IS_DOCKER', False)
+if is_docker:
+    csv_path = '/mounted/data/'
+    model_path = '/mounted/models/'
+
+
+train_model = Blueprint('train_model', __name__)
+
+@train_model.route('/train-model-and-save')
+def train_model_and_save():
+    X, y = get_X_y()
+    pipeline = get_ppipeline()
+    pipeline.fit(X, y)
+    joblib.dump(pipeline, model_path + 'pipeline.pkl')
+
+
+def get_X_y():
+    files_with_dates = []
+    last_files = None
+
+    for file_name in os.listdir(csv_path):
+        path = csv_path + file_name
+        if os.path.isfile(path):
+            files_with_dates.append((os.path.getmtime(path), path))
+
+    files_with_dates.sort(key=lambda el: el[0], reverse=True)
+    last_files = list(map(lambda x: x[1], files_with_dates[:8]))
+
+    dfs_to_concat = []
+
+    for file in last_files:
+        dfs_to_concat.append(pd.read_csv(file, delimiter=';'))
+
+    df = pd.concat(dfs_to_concat)
+    df.drop_duplicates('id', inplace=True)
+
+    df = df.rename(columns={'option1': 'condition', 'option2': 'house_type', 'option3': 'balcony', 'option4': 'parking'})
+
+    df = preprocess_data(df)
+
+    df = df.drop(columns=[
+        'area_living', 'id', 'author_id', 'address', 'user_address', 'created_at',
+        'last_time_up', 'url', 'photo', 'seller', 'price_byn',
+        'resale'
+    ])
+
+    y = df['price_usd'].to_numpy()
+    X = df.drop(columns=['price_usd'])
+
+    return X, y
+
+
+def preprocess_data(X):
+    X['year'] = X['house_type'].str.extract('(\d+)')
+    X['balcony'] = X['balcony'].str.contains('балконом').astype('int')
+    X['house_type'] = X['house_type'].apply(lambda s: s.split(' дом')[0])
+    X['parking'] = X['parking'].str.contains('выделенным парковочным местом').astype('int')
+    X['year'] = X['year'].fillna(-999)
+    X['area_kitchen'] = X['area_kitchen'].fillna(-999)
+    X['year'] = X['year'].astype('int')
+    X['first_floor'] = (X['floor'] == 1).astype(int)
+    X['last_floor'] = (X['floor'] == X['number_of_floors']).astype(int)
+    X['is_new'] = X['condition'].str.contains('Новостройка').astype('int')
+    X['renovation'] = X['condition'].str.contains('с ремонтом').astype('int')
+    X['house_type'] = X['house_type'].replace({'Панельный': 'panel',
+                                               'Блочный': 'block',
+                                               'Кирпичный': 'brick',
+                                               'Монолитный': 'monolit'}).astype('category')
+
+    X.drop(columns=['condition'], inplace=True)
+
+    X = X[X['area_total'] < 200]
+    X = X[X['price_usd'] < 175000]
+    X = X[X['price_usd'] > 32000]
+    X = X[X['area_kitchen'] < 30]
+    X = X[X['number_of_rooms'] < 5]
+    #     X = X[X['dist_from_center'] < 0.25]
+
+    return X
+
+def get_ppipeline():
+    one_hot_cols = ['house_type', 'number_of_rooms']
+    scale_cols = ['latitude',
+                  'longitude',
+                  'floor',
+                  'number_of_floors',
+                  'number_of_rooms',
+                  'area_total',
+                  'area_kitchen',
+                  'year']
+
+    col_transformer = make_column_transformer(
+        (OneHotEncoder(sparse=False, handle_unknown='ignore'), one_hot_cols),
+        (MinMaxScaler(), scale_cols),
+        remainder='passthrough', verbose=True)
+
+    lgbm = LGBMRegressor(learning_rate=0.02,
+                         num_leaves=41,
+                         n_estimators=2000,
+                         max_depth=10,
+                         colsample_bytree=0.62,
+                         reg_lambda=0.81)
+    rf = RandomForestRegressor(n_estimators=250, max_features=12, bootstrap=False, n_jobs=-1)
+
+    xgb = XGBRegressor(learning_rate=0.02,
+                       num_leaves=41,
+                       n_estimators=2000,
+                       max_depth=10,
+                       colsample_bytree=0.62,
+                       reg_lambda=0.81,
+                       n_jobs=-1)
+
+    voter = VotingRegressor([
+        ('rf', rf),
+        ('lgbm', lgbm),
+        ('xgb', xgb),
+    ], n_jobs=-1)
+
+    pipeline = Pipeline([
+        ('custom_transformer', CustomTransformer()),
+        ('col_transformer', col_transformer),
+        ('estimator', voter)
+    ], verbose=True)
+
+    return pipeline
+
+if __name__ == "__main__":
+    train_model_and_save()
